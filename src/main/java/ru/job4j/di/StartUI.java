96ccadf9b1576b827372e634b971b973bc45371e
apereslavtsev <87319362+apereslavtsev@users.noreply.github.com>
2023-09-18 18:39:08 +0000

    4. Время жизни (Scope) [#261277]

--- src/main/java/ru/job4j/di/StartUI.java
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -1,11 +1,13 @@
 package ru.job4j.di;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+@Scope("prototype")
 @Component
 public class StartUI {
-    
+
     @Autowired
     private Store store;
 
--- src/main/java/ru/job4j/di/Store.java
+++ src/main/java/ru/job4j/di/Store.java
@@ -3,8 +3,10 @@ package ru.job4j.di;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+@Scope("prototype")
 @Component
 public class Store {
 
